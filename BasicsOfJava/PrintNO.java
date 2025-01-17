a22c1092370e37c50a0274626f0542a540ad2f43
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-17 20:47:42 +0530

    while or for loop

--- BasicsOfJava/PrintNO.java
+++ BasicsOfJava/PrintNO.java
@@ -5,5 +5,10 @@ public class PrintNO {
     for (int i = 0; i < 10; i++) {
       System.out.println(i+1);
     }
+    int i = 1;
+    while (i <= 10) {
+      System.out.println(i);
+      i++;
+    }
   }
 }
