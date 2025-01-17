fa6eeab02ed1dc234d76fa11531a1de515322961
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-17 20:57:57 +0530

    print square

---
+++ BasicsOfJava/PrintSquare.java
@@ -0,0 +1,9 @@
+package BasicsOfJava;
+
+public class PrintSquare {
+  public static void main(String args[]){
+    for(int lines = 1; lines <= 4; lines++){
+      System.out.println("****");
+    }
+  }
+}
