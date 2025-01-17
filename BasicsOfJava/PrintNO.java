18f65261411e8b5ee537ff1be7a54a5b0f23b55b
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-17 20:53:15 +0530

    print sum

--- BasicsOfJava/PrintNO.java
+++ BasicsOfJava/PrintNO.java
@@ -5,9 +5,9 @@ public class PrintNO {
     for (int i = 0; i < 10; i++) {
       System.out.println(i+1);
     }
-    int i = 1;
+    int i = 1, sum=0;
     while (i <= 10) {
-      System.out.println(i);
+      System.out.println(sum += i);
       i++;
     }
   }
