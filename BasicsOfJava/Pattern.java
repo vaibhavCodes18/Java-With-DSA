d20818ac3fb2da2819cc931b23402c0703c050b5
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-18 22:35:12 +0530

    Half pyramid done

--- BasicsOfJava/Pattern.java
+++ BasicsOfJava/Pattern.java
@@ -1,7 +1,7 @@
 public class Pattern {
-  public static void main(String args[]) {
+  public static void main(String args[]){
 
-    for (int i = 1; i <= 4+1; i++) {
+    for (int i = 1; i <= 4; i++) {
       for (int j = 1; j <= i; j++) {
         System.out.print("*");
       }
