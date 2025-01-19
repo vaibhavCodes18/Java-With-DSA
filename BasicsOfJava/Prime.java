49e3751e8c412e8286dbf2d661112a548f97b30d
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-19 23:46:25 +0530

    optimized prime number

--- BasicsOfJava/Prime.java
+++ BasicsOfJava/Prime.java
@@ -14,7 +14,19 @@ public class Prime {
     return true;
   }
 
+  public static boolean isPrimeoptimized(int n) {
+    if (n == 2) {
+      return true;
+    }
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   public static void main(String[] args) {
-    System.out.println(isPrime(4));
+    System.out.println(isPrimeoptimized(3));
   }
 }
