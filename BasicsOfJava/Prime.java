7512c9a92aa5f3216101f6c265e374386eb6cfff
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-19 23:26:36 +0530

    prime number

--- BasicsOfJava/Prime.java
+++ BasicsOfJava/Prime.java
@@ -1,14 +1,17 @@
 public class Prime {
 
   public static boolean isPrime(int n) {
-    boolean isPrime = true;
-    for (int i = 2; i < n ; i++) {
+    // boolean isPrime = true;
+    if (n == 2) {
+      return true;
+    }
+
+    for (int i = 2; i < n; i++) {
       if (n % i == 0) {
-        isPrime = false;
-        break;
+        return false;
       }
     }
-    return isPrime;
+    return true;
   }
 
   public static void main(String[] args) {
