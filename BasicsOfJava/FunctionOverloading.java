0ecd7f488edcec44360d93ec4665663b876101d7
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-19 22:38:33 +0530

    Function Overloading

---
+++ BasicsOfJava/FunctionOverloading.java
@@ -0,0 +1,12 @@
+public class FunctionOverloading {
+  public static int sum(int a, int b){
+    return a+b;
+  }
+  public static int sum(int a, int b,int c){
+    return a+b+c;
+  }
+  public static void main(String[] args){
+    System.out.println(sum(5,5));
+    System.out.println(sum(5,5, 5));
+  }
+}
