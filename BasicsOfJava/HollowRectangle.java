1d55156ddaf9fc739388392f3dab4003e9ffd7da
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-21 21:55:18 +0530

    Hollow rectangle done

--- BasicsOfJava/HollowRectangle.java
+++ BasicsOfJava/HollowRectangle.java
@@ -1,21 +1,23 @@
 public class HollowRectangle {
 
-  public static void Pattern(int totRow, int totCols){
-    for(int i = 1; i <= totRow; i++){
+  public static void hollowRectanle(int totRows, int totCols){
+
+    for(int i = 1; i <= totRows; i++){
       for(int j = 1; j <= totCols; j++){
-        if(i == 1 || i == totRow || j == 1 || j == totCols){
+        if(i == 1 || i == totRows || j == 1 || j == totCols){
           System.out.print("*");
         }else{
           System.out.print(" ");
         }
+      }
+      System.out.println();
     }
-    System.out.println();
-  }
+
   }
 
   public static void main(String[] args){
-
-    Pattern(4, 5);
-    
+    int rows = 4;
+    int cols = 5;
+    hollowRectanle(rows, cols);
   }
 }
