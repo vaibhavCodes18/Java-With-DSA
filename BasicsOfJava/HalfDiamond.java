4bc396252f684f74d69966ca8da4f7c09fd0ded2
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-24 17:09:13 +0530

    halfDiamond

---
+++ BasicsOfJava/HalfDiamond.java
@@ -0,0 +1,24 @@
+public class HalfDiamond{
+
+  public static void halfDiamond(int n){
+    // rows
+    for(int i = 1; i <= n; i++){
+      // spaces
+      for(int j = 1; j <=n-i; j++){
+        System.out.print(" ");
+      }
+
+      // stars
+      for(int k = 1; k <= (2*i-1); k++){
+        System.out.print("*");
+      }
+      
+      System.out.println();
+    }
+  }
+
+
+  public static void main(String[] args) {
+    halfDiamond(4);
+  }
+}
