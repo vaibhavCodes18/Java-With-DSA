cd2e927d77c530be104e487e0bd205e9245b2aae
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-25 22:30:46 +0530

    LargestNumber

--- LinearSearch/LargestNumber.java
+++ LinearSearch/LargestNumber.java
@@ -4,13 +4,18 @@ public class LargestNumber {
 
   public static int largestNum(int numbers[]){
     int largest = Integer.MIN_VALUE;
+    int smallest = Integer.MAX_VALUE;
 
     for(int i = 0; i < numbers.length; i++){
       if(numbers[i] > largest){
         largest = numbers[i];
+        index = i;
+      }
+      if(smallest > numbers[i]){
+        smallest = numbers[i];
       }
     }
-
+    System.out.println("Smallest number is " + smallest);
     return largest;
   }
                               
