1f14b66263da8c2d7daebe96eacfc2031c3085d8
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-25 20:31:09 +0530

    LargestNumber

--- LinearSearch/LargestNumber.java
+++ LinearSearch/LargestNumber.java
@@ -3,7 +3,7 @@ package LinearSearch;
 public class LargestNumber {
 
   public static int largestNum(int numbers[]){
-    int largest = -1;
+    int largest = Integer.MIN_VALUE;
 
     for(int i = 0; i < numbers.length; i++){
       if(numbers[i] > largest){
@@ -15,7 +15,7 @@ public class LargestNumber {
   }
 
   public static void main(String[] args){
-    int numbers[] = {1, 4, 7, 9, 8, 45};
+    int numbers[] = {1, 4, 87, 9, 8, 45};
 
     int max = largestNum(numbers);
     System.out.println("Largest number in the array is: " + max);
