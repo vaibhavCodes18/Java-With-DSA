e5a05805ee542bc0dee97154b9f4bebf33549386
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-25 12:21:04 +0530

    PalindromePyramid

---
+++ BasicsOfJava/PalindromePyramid.java
@@ -0,0 +1,10 @@
+public class PalindromePyramid {
+
+  public static void panildromePyramid(int n){
+    System.out.println(n);
+  }
+
+  public static void main(String[] args){
+    panildromePyramid(4);
+  }
+}
