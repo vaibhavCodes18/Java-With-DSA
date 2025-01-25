9db927aa2c67805fa1e048e2aa8745f45bc16f5e
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-25 18:04:20 +0530

    Array update

--- Array/ArrayCC.java
+++ Array/ArrayCC.java
@@ -1,20 +1,17 @@
-package Array;
-
-import java.util.Scanner;
-
 public class ArrayCC {
-  public static void main(String[] args) {
-    int marks[] = new int[50];
-    Scanner sc = new Scanner(System.in);
-  
-    marks[0] = sc.nextInt();
-    marks[1] = sc.nextInt();
-    marks[2] = sc.nextInt();
 
-    System.out.println("phy: " + marks[0]);
-    System.out.println("chem: " + marks[1]);
-    System.out.println("maths: " + marks[2]);
+  public static void update(int marks[]){
+    for(int i = 0; i < marks.length; i++){
+      marks[i] += 1;
+    }
   }
 
-  
+  public static void main(String[] args) {
+    int marks[] = {67, 88, 99};
+    update(marks);
+
+    for(int i = 0; i < marks.length; i++){
+      System.out.println(marks[i]);
+    }
+  }
 }
