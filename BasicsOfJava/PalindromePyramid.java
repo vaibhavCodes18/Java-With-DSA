03c6d279a6c8dbc5d7845bb4d427a1e64a29e277
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-25 12:51:10 +0530

    PalindromePyramid

--- BasicsOfJava/PalindromePyramid.java
+++ BasicsOfJava/PalindromePyramid.java
@@ -1,7 +1,23 @@
 public class PalindromePyramid {
 
   public static void panildromePyramid(int n){
-    System.out.println(n);
+    
+    // rows
+    for(int i = 1; i <= n; i++){
+      // spaces
+      for(int j = 1; j <= n-i; j++){
+        System.out.print(" ");
+      }
+      // numbers - desc
+      for(int k = i; k >= 1; k--){
+        System.out.print(k);
+      }
+      // numbers - asc
+      for(int j = 2; j <= i; j++){
+        System.out.print(j);
+      }
+      System.out.println();
+    }
   }
 
   public static void main(String[] args){
