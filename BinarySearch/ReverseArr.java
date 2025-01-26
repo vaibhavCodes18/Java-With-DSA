bd927285324d0eb5d412d1d294f3d81002efe84d
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-26 14:05:40 +0530

    ReverseArr

---
+++ BinarySearch/ReverseArr.java
@@ -0,0 +1,25 @@
+// package BinarySearch;
+
+public class ReverseArr {
+
+  public static void reverseArr(int[] arr){
+    int start = 0, end = arr.length-1;
+
+    while (start < end) {
+      int temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+    }
+  }
+
+  public static void main(String[] args) {
+      int[] arr = {1, 2, 3, 4, 5, 6, 89, 90};
+
+      reverseArr(arr);
+      for(int i = 0; i < arr.length; i++){
+        System.out.print(arr[i] + " ");
+      }
+  }
+}
