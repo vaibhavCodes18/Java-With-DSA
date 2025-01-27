2d7d599de18882605b4f223da80af963dd2e9e9a
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-27 23:54:12 +0530

    maxSum

--- BinarySearch/PrefixSum.java
+++ BinarySearch/PrefixSum.java
@@ -2,8 +2,26 @@ package BinarySearch;
 
 public class PrefixSum {
   
-  public static void prefixSum(int[] args){
-    
+  public static void prefixSum(int[] arr){
+    int currSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+    int[] prefix = new int[arr.length];
+
+    prefix[0] = arr[0];
+    for(int i = 1; i < prefix.length; i++){
+      prefix[i] = prefix[i-1] + arr[i];
+    }
+
+    for(int i = 0; i < arr.length; i++){
+      for(int j = i; j < arr.length; j++){
+        currSum = 0;
+
+        if(currSum > maxSum){
+          maxSum = currSum;
+        }
+      }
+    }
+    System.out.println("Max sum = " + maxSum);
   }
 
   public static void main(String[] args){
