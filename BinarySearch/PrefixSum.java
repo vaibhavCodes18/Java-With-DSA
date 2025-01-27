d5493a9f671470ffcec466ee10d83827fefadd20
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-27 23:49:30 +0530

    prefixSum

---
+++ BinarySearch/PrefixSum.java
@@ -0,0 +1,14 @@
+package BinarySearch;
+
+public class PrefixSum {
+  
+  public static void prefixSum(int[] args){
+    
+  }
+
+  public static void main(String[] args){
+    int[] arr = {2, 4, 6, 8, 10};
+
+    prefixSum(arr);
+  }
+}
