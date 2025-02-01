a34ab6123ecbbce42a7621609bbe2a4ff38b84e8
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-02-02 00:18:20 +0530

    Buy and sell 121 leetcode

---
+++ LeetCode/BuySellStocks.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class BuySellStocks {
+
+  public static int maxProfit(int[] prices){
+    int buyPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+
+    
+    for(int i = 0; i < prices.length; i++){
+      if(buyPrice < prices[i]){
+        int profit = prices[i] - buyPrice;
+        maxProfit = Math.max(maxProfit, profit);
+      }
+      else{
+        buyPrice = prices[i];
+      }
+    }
+    return maxProfit;
+  }
+
+  public static void main(String[] args) {
+    int[] prices = {7,1,5,3,6,4};
+    int result = maxProfit(prices);
+    System.out.print(result);
+  }
+}
