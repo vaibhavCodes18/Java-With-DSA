4d364f59f1f617e94faae509239102430a8bf538
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-04-11 23:38:27 +0530

    add new one

---
+++ BasicsOfJava/printUniquePattern.java
@@ -0,0 +1,19 @@
+public class printUniquePattern {
+  public static void main(String[] args){
+    int n = 4;
+    int num = 1; 
+
+    for(int i = 1; i <= n; i++){
+      for(int j = 1; j <= i; j++){
+        System.out.print(num + " ");
+        if(i == j){
+          num++;
+        }
+        else{
+          num += 2;
+        }
+      }
+      System.out.println();
+    }
+  }
+}
