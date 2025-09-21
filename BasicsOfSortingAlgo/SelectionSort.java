2ea6a0423fd34dd61750d32f94fe6b97b892e862
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-09-21 23:25:00 +0530

    Selction sort done

--- BasicsOfSortingAlgo/SelectionSort.java
+++ BasicsOfSortingAlgo/SelectionSort.java
@@ -4,16 +4,16 @@ class SelectionSort{
 		int n = arr.length;
 		
 		for(int i = 0; i < n-1; i++){
-			int smallest = i;
+			int minPos = i;
 			for(int j = i+1; j < n; j++){
-				if(arr[smallest] > arr[j]){
-					smallest = j;
+				if(arr[minPos] > arr[j]){
+					minPos = j;
 				}
 			}
 			
 			int temp = arr[i];
-			arr[i] = arr[smallest];
-			arr[smallest] = temp;
+			arr[i] = arr[minPos];
+			arr[minPos] = temp;
 		}
 		for(int i = 0; i < n; i++){
 			System.out.println(arr[i]);
