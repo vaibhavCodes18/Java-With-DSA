62d18f2862df2c61ec1fb24b951a8baae4f3c294
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-09-22 22:45:57 +0530

    counting sort

--- BasicsOfSortingAlgo/Sorting.java
+++ BasicsOfSortingAlgo/Sorting.java
@@ -38,6 +38,28 @@ class Sorting{
 		}
 	}
 	
+	public static void CountingSort(int[] arr){
+		int[] largest = Integer.MIN_VALUE;
+		
+		for(int i = 0; i < arr.length; i++){
+			largest = Math.max(largest, arr[i]);
+		}
+		
+		int[] count = new int[largest+1];
+		for(int i = 0; i < arr.length; i++){
+			count[arr[i]]++;
+		}
+		
+		int j = 0;
+		for(int i = 0; i<count.length; i++){
+			while(count[i]>0){
+				arr[j] = i;
+				j++;
+				count[i]--;
+			}
+		}
+	}
+	
 	public static void pritnArr(int[] arr){
 		for(int i = 0; i < arr.length; i++){
 			System.out.println(arr[i]);
--- BasicsOfSortingAlgo/countingSort.java
+++ BasicsOfSortingAlgo/countingSort.java
@@ -1,24 +1,25 @@
-class countingSort{
+class CountingSort{
 	public static void pritnArr(int[] arr){
 		for(int i = 0; i < arr.length; i++){
 			System.out.println(arr[i]);
 		}
 	}
-	public static void main(String[] ar){
-		int[] arr = {4, 5, 6, 7, 2, 1};
+	public static void main(String[] args){
+		int[] arr = {1, 4, 1, 3, 2, 4, 3, 7};
 		int largest = Integer.MIN_VALUE;
 		
-		for(int i = 0; i<arr.length; i++){
+		for(int i = 0; i < arr.length; i++){
 			largest = Math.max(largest, arr[i]);
 		}
 		
 		int[] count = new int[largest+1];
-		for(int i = 0; i<arr.length; i++){
+		for(int i = 0; i < arr.length; i++){
 			count[arr[i]]++;
 		}
+		
 		int j = 0;
-		for(int i = 0; i<count.length;i++){
-			while(count[i] > 0){
+		for(int i = 0; i <count.length; i++){
+			while(count[i]>0){
 				arr[j] = i;
 				j++;
 				count[i]--;
