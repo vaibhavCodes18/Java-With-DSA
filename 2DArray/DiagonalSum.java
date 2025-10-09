f7175f226805a870389a1efb6a4f729a6ad6a88d
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-10-10 01:00:04 +0530

    Diagonal Sum

---
+++ 2DArray/DiagonalSum.java
@@ -0,0 +1,30 @@
+class DiagonalSum{
+	public static void main(String[] args){
+		int[][] matrix = {
+			{1,2,3,4},
+			{5,6,7,8},
+			{9,10,11,12},
+			{13,14,15,16}
+		};
+		
+		int sum = 0;
+		
+		/*for(int i = 0; i < matrix.length;i++){
+			for(int j = 0; j < matrix[0].length; j++){
+				if(i==j){
+					sum+=matrix[i][j];
+				}
+				else if(i+j == matrix.length-1){
+					sum+=matrix[i][j];
+				}
+			}
+		}*/
+		
+		for(int i = 0; i < matrix.length;i++){
+			sum += matrix[i][i];
+			
+			sum+= matrix[i][matrix[0].length-i-1];
+		}
+		System.out.print(sum);
+	}
+}
