17bba12c73bda4e54d1d16f09f8bf8e4aee6c7e9
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-10-11 22:58:24 +0530

    Stair Search

---
+++ 2DArray/StairSearch.java
@@ -0,0 +1,17 @@
+class StairSearch{
+
+	public static boolean stairSearch(int[][] matrix, int key){
+		
+	}
+	public static void main(String[] args){
+		int[][] matrix = {
+			{10,20,30,40},
+			{15,25,35,45},
+			{27,29,37,48},
+			{32,33,39,50}
+		};
+		int key = 33;
+	
+		stairSearch(matrix, key);
+	}
+}
