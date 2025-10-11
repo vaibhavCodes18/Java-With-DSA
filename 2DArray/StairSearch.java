78622274c05761d1028a982e17ec4f673c51aa82
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-10-11 23:27:14 +0530

    Stair Search done

--- 2DArray/StairSearch.java
+++ 2DArray/StairSearch.java
@@ -1,7 +1,22 @@
 class StairSearch{
 
 	public static boolean stairSearch(int[][] matrix, int key){
+		int row = 0, col = matrix[0].length-1;
 		
+		while(row < matrix.length && col >= 0){
+			if(key == matrix[row][col]){
+				System.out.print("Key is found at (" + row + ", " + col + ")");
+				return true;
+			}
+			else if(key < matrix[row][col]){
+				col--;
+			}
+			else if(key > matrix[row][col]){
+				row++;
+			}
+		}
+		System.out.print("Key is not found");
+		return false;
 	}
 	public static void main(String[] args){
 		int[][] matrix = {
